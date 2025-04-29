203ab1537f1b3ec3f2624cd22301bdb52192f16f
nmiller95 <nikkijanemiller@gmail.com>
2025-04-29 18:15:44 +0200

    remove large linelists

--- README.md
+++ README.md
@@ -1,23 +1,77 @@
-# TurboSpectrum-Wrapper
-Author: Ekaterina Magg
-MPIA, Heidelberg, Germany
+# TurboSpectrum-Wrapper for M-dwarf SAPP
 
-in collaboration with M. Bergemann, B. Plez, J. Gerber
+[Original version](https://github.com/EkaterinaSe/TurboSpectrum-Wrapper) by E. Magg, M. Bergemann, B. Plez, J. Gerber.
+Before use, please refer to the 
+[original documentation](https://ekaterinase.github.io/TurboSpectrum-Wrapper/build/html/index.html).
 
-welcome!
-to use the wrapper please make sure you have TurboSpectrum installed
+This modified version is specifically tailored for the purpose for generating a grid of synthetic spectra for training a 
+neural network for the SAPP pipeline (see 
+[Kovalev+19](https://ui.adsabs.harvard.edu/abs/2019A%26A...628A..54K/abstract), 
+[Gent+22](https://ui.adsabs.harvard.edu/abs/2022A%26A...658A.147G/abstract), 
+[Olander+25](https://ui.adsabs.harvard.edu/abs/2025A%26A...696A..62O/abstract)).
 
-you can obtain the latest (NLTE!) version of TurboSpectrum here:
-https://github.com/bertrandplez/Turbospectrum2020.git
+## Installation 
 
-### before you get started:
-please make sure to read the conditions and applied licenses below:
+### This wrapper
+Clone this repository:
+```bash
+git clone https://github.com/nmiller95/TurboSpectrum-Wrapper.git 
+```
+Make sure the Python dependencies are installed. This can be done manually or install with:
+```bash
+pip install -r requirements.txt
+```
 
-This code is open-source and it is distributed under GNU GPLv3 license.
+#### Configuration
+You can customise several aspects of the wrapper in `/input/config.txt`, e.g. number of CPUs to use,
+the wavelength range over which to generate your spectra, and file paths.
 
-If you would like to make any module of the provided here software a part of other stand-alone projects, 
-please contact me via emagg at mpia de
+### TurboSpectrum
+Make sure you have [TurboSpectrum v.20](https://github.com/bertrandplez/Turbospectrum2020)
+installed. If you installing it specifically for this project, you may find it convenient to place the files in the 
+`/turbospectrum` directory in this project.
 
-for detailed documentation please see:
-https://ekaterinase.github.io/TurboSpectrum-Wrapper/
+Update the variable `ts_root` in `/input/config.txt` to match the *absolute* path of your TurboSpectrum installation.
 
+#### Known issues
+Since this repo is a quick modification of the `TurboSpectrum-Wrapper` code, I didn't fix all of the problems. 
+If the code crashes when you run it for the first time, check that the compiler on lines 51 and 135 of `run_ts.py` 
+match your fortran compiler. For my Mac with gfortran, I changed 'exec' to 'exec-gf'. 
+Available compiler options are: 'exec', 'exec-gf', 'exec-ifx', 'exec-intel'.
+
+### MARCS model atmospheres
+Download MARCS models over the parameter range you are interested in and place them in `/input/marcs_models`.
+These can be downloaded from the [main site](https://marcs.oreme.org/) or [Uppsala mirror](https://marcs.astro.uu.se/).
+You will only need the `.mod` files.
+
+If you prefer to place these elsewhere, you can update the variable `atmos_path` in `/input/config.txt`
+
+### Line lists
+Line lists suitable for generating M-dwarf spectra have been included in the `/input/linelists` directory. 
+Place or replace line lists here and update the variable `linelist` in `/input/config.txt`.
+
+
+## Running the wrapper
+
+### Random grid parameter generation
+
+To generate a file containing a set of random parameters, navigate to the script `/source/make_random_grid_input.py`.
+You'll need to go to the bottom of the script to tailor the parameters you want to use and their ranges.
+Here, you will find some guidance. Once happy with the setup, navigate to the `/source` directory and run: 
+```bash
+python make_random_grid_input.py
+```
+
+Check that the correct file name/path is in `/input/config.txt` before proceeding to the next step.
+
+### Running TurboSpectrum
+
+Now you can run the whole thing. Make sure you're in `/source` directory. Basic usage:
+```bash
+python generate_random_grid.py
+```
+This assumes you're using `/input/config.txt` as the configuration file.
+You can instead customise the configuration file path and job names with:
+```bash
+python generate_random_grid.py configFile.txt jobName
+```
---
+++ source/make_random_grid_input.py
@@ -0,0 +1,222 @@
+# A friendly script to generate your random input parameters file
+# NJM - 29.04.2025
+
+import random
+
+
+class SpectrumGridGenerator:
+    def __init__(self, limits: dict):
+        """
+        Generate random grid of stellar parameters compatible with Turbospectrum. Uses Magg+22 solar abundances.
+
+        Args:
+            limits(dict): Dictionary of tuples (low, high) for each parameter. Must contain (Teff, logg, Vturb, Z),
+                            may also contain a number of abundances in [X/Fe]
+        """
+        self.limits = limits
+        self.existing_params = {}
+
+        # Limits taken from scripts in TASS
+        self.similarity_tol = {
+            "Teff": 5,
+            "logg": 0.05,
+            "Vturb": 0.01,
+            "Z": 0.001,
+            "abundances": 0.001
+        }
+
+        # Recommended solar abundances taken from Magg+22
+        self.solar_abund = {
+            'C': 8.56,
+            'N': 7.98,
+            'O': 8.77,
+            'F': 4.40,
+            'Ne': 8.15,
+            'Na': 6.29,
+            'Mg': 7.55,
+            'Al': 6.43,
+            'Si': 7.59,
+            'P': 5.41,
+            'S': 7.16,
+            'K': 5.14,
+            'Ca': 6.37,
+            'Sc': 3.07,
+            'Ti': 4.94,
+            'V': 3.95,
+            'Cr': 5.74,
+            'Mn': 5.52,
+            'Fe': 7.50,
+            'Co': 4.95,
+            'Ni': 6.24,
+        }
+
+    def _is_duplicate(self, candidate: dict):
+        """
+        Check if the candidate spectrum is identical to an existing spectrum within tolerance limits
+
+        Args:
+            candidate (dict): Dictionary containing parameters of candidate spectrum
+        """
+        n = len(next(iter(self.existing_params.values()), []))
+        if n == 0:
+            return False
+        varying_params = {k for k, (lo, hi) in self.limits.items() if lo != hi}
+
+        for i in range(n):
+            match = True
+            for param in varying_params:
+                val1 = candidate.get(param)
+                val2 = self.existing_params.get(param, [None]*n)[i]
+
+                if val1 is None or val2 is None:
+                    match = False
+                    break
+
+                tol = self.similarity_tol.get(param, self.similarity_tol.get("abundances", 1e-6))
+                if abs(val1 - val2) > tol:
+                    match = False
+                    break
+
+            if match:
+                return True
+        return False
+
+    def _append_candidate(self, candidate: dict):
+        """
+        Append candidate set of spectral parameters to the existing set.
+
+        Args:
+            candidate (dict): Dictionary containing parameters of candidate spectrum
+        """
+        for key, value in candidate.items():
+            if key not in self.existing_params:
+                self.existing_params[key] = []
+            self.existing_params[key].append(value)
+
+    def _convert_abundances(self, candidate: dict):
+        """
+        Convert [X/Fe] to log ε(X) using [Fe/H] and solar ε(X)
+
+        Args:
+            candidate (dict): Dictionary containing parameters of candidate spectrum
+        """
+        feh = candidate["FeH"]
+        log_eps = {}
+        for elem, xfe in candidate.items():
+            if elem in self.solar_abund and elem != "Fe":
+                solar_val = self.solar_abund[elem]
+                log_eps[elem] = solar_val + feh + xfe
+        log_eps["Fe"] = self.solar_abund["Fe"] + feh
+        return log_eps
+
+    def sample_one(self, max_attempts=1000):
+        """
+        Generate one random set of spectral parameters
+
+        Args:
+            max_attempts (int): Maximum number of attempts to generate
+        """
+        for _ in range(max_attempts):
+            candidate = {}
+            for param, (lo, hi) in self.limits.items():
+                if hi == lo:
+                    candidate[param] = lo
+                else:
+                    candidate[param] = random.uniform(lo, hi)
+            if not self._is_duplicate(candidate):
+                self._append_candidate(candidate)
+                return candidate
+        return None
+
+    def generate_samples(self, n: int):
+        """
+        Generate n random sets of spectral parameters, checking for and removing duplicates.
+
+        Args:
+            n (int): Number of spectra to generate
+        """
+        samples = []
+        for _ in range(n):
+            raw = self.sample_one()
+            if raw:
+                logeps = self._convert_abundances(raw)
+                merged = {**raw, **logeps}
+                samples.append(merged)
+        return samples
+
+    def export_txt(self, samples: list, filename: str):
+        """
+        Write space-separated .txt file with headers and custom formatting. Last column is fixed to H = 12.0.
+
+        Args:
+            samples (list): List containing parameters of the grid of spectral parameters
+            filename (str): Filename of output file
+        """
+        order = ("Teff", "logg", "Vturb", "FeH")  # Mandatory parameters, will always be given
+        excluded = set(order)
+        abundances_order = [key for key in self.limits if key not in excluded]
+
+        with open(filename, "w") as f:
+            # Write header
+            header = list(order) + abundances_order + ["H"]
+            f.write(" ".join(header) + "\n")
+
+            for sample in samples:
+                row = []
+
+                # Stellar parameters - rounding where appropriate
+                for param in order:
+                    val = sample.get(param, 0)
+                    if param == "Teff":
+                        row.append(f"{val:.0f}")
+                    elif param == "logg" or param == "Vturb":
+                        row.append(f"{val:.2f}")
+                    elif param == "FeH":
+                        row.append(f"{val:.3f}")
+                    else:
+                        row.append(f"{val:.6f}")
+
+                # Abundances - rounding where appropriate
+                for elem in abundances_order:
+                    row.append(f"{sample.get(elem, 0):.4f}")
+
+                # Add hydrogen last
+                row.append("12.0")
+
+                f.write(" ".join(row) + "\n")
+
+
+if __name__ == "__main__":
+
+    # Number of spectra to generate
+    n_spectra = 50
+
+    # Lower, upper limits to place on each parameter.
+
+    # Teff, logg, vturb and [Fe/H] MUST be specified. The other parameters may be added/removed.
+    # To fix at a single value, let lower=upper.
+    # Give [X/Fe] relative abundances - they will be converted to absolute values using Magg+22 solar abundances
+    grid_limits = {
+        'Teff': (2500, 4500),
+        'logg': (4.3, 5.3),
+        'Vturb': (0.01, 2.0),
+        'FeH': (-2.0, 0.6),
+        # 'C': (-0.2, 0.8),  # 14 elements as per Souto+22
+        'O': (-0.2, 0.8),
+        # 'Na': (-0.2, 0.8),
+        'Mg': (-0.2, 0.8),
+        # 'Al': (-0.2, 0.8),
+        'Si': (-0.2, 0.8),
+        # 'K': (-0.2, 0.8),
+        'Ca': (-0.2, 0.8),
+        'Ti': (-0.2, 0.8),
+        # 'V': (-0.2, 0.8),
+        # 'Cr': (-0.2, 0.8),
+        # 'Mn': (-0.2, 0.8),
+        # 'Fe': (-0.2, 0.8),
+        # 'Ni': (-0.2, 0.8)
+    }
+
+    gen = SpectrumGridGenerator(grid_limits)
+    grid = gen.generate_samples(n_spectra)
+    gen.export_txt(grid, "../input/random_grid.txt")
